669fa47fbfdb48833a23faf948e5ad119d50cbde
P.NIVETHITHA <nivethitha3502@gmail.com>
2026-03-16 21:55:36 +0530

    Add files via upload

---
+++ TASK10_StudentCRUDApp/sqlfile.sql
@@ -0,0 +1,12 @@
+drop database studentsdb;
+CREATE DATABASE studentsdb;
+USE studentsdb;
+
+CREATE TABLE studentsnew (
+    student_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    department VARCHAR(50),
+    age INT
+);
+
+SELECT * FROM studentsnew;
